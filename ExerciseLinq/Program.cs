c2d8f6e59ee92c6b4d97c2afff088d351e0e420c
thiago jose da silva <thiago.ads.silva@gmail.com>
2023-05-08 16:51:39 -0300

    Organização

---
+++ ExerciseLinq/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ultilizing the Linq
+
+// Specify the data source
+int[] numbers = new int[] { 1, 2, 3, 4, 5 };
+
+// Define the query expression
+var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+// Exwcute the query
+foreach (int x in result)
+{
+    Console.WriteLine(x);
+}
